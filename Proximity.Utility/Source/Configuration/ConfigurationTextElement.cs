d73874abde189b5b3d160b7c09a2869561f3f3fb
alphagremlin <alphagremlin@503ed804-8c27-425f-8995-0214f978a1d5>
2014-06-13 06:10:24 +0000

    Added configuration element that can hold text content
    git-svn-id: https://iproximity.svn.cloudforge.com/libraries/trunk@30 503ed804-8c27-425f-8995-0214f978a1d5

---
+++ Proximity.Utility/Source/Configuration/ConfigurationTextElement.cs
@@ -0,0 +1,79 @@
+﻿/****************************************\
+ ConfigurationTextElement.cs
+ Created: 2014-06-13
+\****************************************/
+using System;
+using System.Configuration;
+using System.Xml;
+//****************************************
+
+namespace Proximity.Utility.Configuration
+{
+	/// <summary>
+	/// Represents a configuration element within a configuration file that also contains a text node
+	/// </summary>
+	public class ConfigurationTextElement : ConfigurationElement
+	{	//****************************************
+		private string _Content;
+		//****************************************
+		
+		/// <summary>
+		/// Creates a new configuration text element
+		/// </summary>
+		public ConfigurationTextElement() : base()
+		{
+		}
+		
+		//****************************************
+		
+		/// <inheritdoc />
+		protected override void DeserializeElement(XmlReader reader, bool serializeCollectionKey)
+		{
+			while (reader.MoveToNextAttribute())
+			{
+				var MyProperty = Properties[reader.LocalName];
+				
+				SetPropertyValue(MyProperty, reader.Value, true);
+			}
+			
+			reader.MoveToElement();
+			
+			if (reader.IsEmptyElement)
+			{
+				_Content = null;
+			}
+			else
+			{
+				_Content = reader.ReadElementContentAsString();
+			}
+		}
+		
+		/// <inheritdoc />
+		protected override bool SerializeElement(XmlWriter writer, bool serializeCollectionKey)
+		{
+			foreach(ConfigurationProperty MyProperty in Properties)
+			{
+				var MyResult = this[MyProperty];
+				
+				if (MyResult != null)
+					writer.WriteAttributeString(MyProperty.Name, MyResult.ToString());
+			}
+			
+			if (_Content != null)
+				writer.WriteString(_Content);
+			
+			return true;
+		}
+		
+		//****************************************
+		
+		/// <summary>
+		/// Gets/Sets the text content of this element
+		/// </summary>
+		public string Content
+		{
+			get { return _Content; }
+			set { _Content = value; }
+		}
+	}
+}
